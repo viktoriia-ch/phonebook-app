972e860998e640a46bbb9a7d67fc136c37ea9d52
viktoriia-ch <82337625+viktoriia-ch@users.noreply.github.com>
2023-03-11 13:53:07 +0000

    Deploying to gh-pages from @ viktoriia-ch/goit-react-hw-08-phonebook@81b35ba53cccb9fa0e5e158d2363b59c79ba9dac 🚀

---
+++ static/js/554.95225efa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[554],{1554:function(e,n,t){t.r(n),t.d(n,{default:function(){return s}});var a=t(4576),o="not-found-page_container__E2hjN",c="not-found-page_image__EcSnp",r=t(184),s=function(){return(0,r.jsx)("div",{className:o,children:(0,r.jsx)("img",{src:a,className:c,alt:"This page is not found!"})})}},4576:function(e,n,t){e.exports=t.p+"static/media/not-found-error.c0bc7d0b97fa8387bee7.png"}}]);
+//# sourceMappingURL=554.95225efa.chunk.js.map
